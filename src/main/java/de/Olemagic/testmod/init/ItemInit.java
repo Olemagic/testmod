7399993adcee04f7a2b8d251ebafb47effc93307
Olemagic <120584379+Olemagic@users.noreply.github.com>
2023-02-13 09:59:13 +0100

    Update ItemInit.java
    Removed Last commit

--- src/main/java/de/Olemagic/testmod/init/ItemInit.java
+++ src/main/java/de/Olemagic/testmod/init/ItemInit.java
@@ -12,15 +12,9 @@ public class ItemInit {
     //Get the item register
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TestmodMain.MODID);
-	//Get the block register
-	public static final DeferredRegister<Block> BLOCKS =
-            DeferredRegister.create(ForgeRegistries.BLOCKS, TestmodMain.MODID);
     //Register Ruby
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
             () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
-	//Register Block of Ruby
-	public static final RegistryObject<Block> RUBY_BLOCK = BLOCKS.register("ruby_block",
-			() -> new Item(new Item.properties().tab(ModCreativeTab.instance)));
 
     public static class ModCreativeTab extends CreativeModeTab {
         private ModCreativeTab(int index, String label) {
