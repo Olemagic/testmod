aff93c33da7c7f9a1c2eb982a740c3e70c973c08
Olemagic <Olemagic24@gmail.com>
2023-02-20 21:05:06 +0100

    added Mango, Coke Coal and Teleport Staff!

--- src/main/java/de/Olemagic/testmod/init/ItemInit.java
+++ src/main/java/de/Olemagic/testmod/init/ItemInit.java
@@ -1,6 +1,11 @@
 package de.Olemagic.testmod.init;
 
 import de.Olemagic.testmod.TestmodMain;
+import de.Olemagic.testmod.items.FuelItem;
+import de.Olemagic.testmod.items.TeleportStaff;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -12,12 +17,29 @@ public class ItemInit {
     //Get the item register
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TestmodMain.MODID);
+
     //Register Ruby
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.instance)));
+            () -> new Item(new Item.Properties().tab(RubyCreativeTab.ruby)));
 
-    public static class ModCreativeTab extends CreativeModeTab {
-        private ModCreativeTab(int index, String label) {
+    //Register Mango
+    public static final RegistryObject<Item> MANGO = ITEMS.register("mango",
+            () -> new Item(new Item.Properties().tab(OtherCreativeTab.other)
+                    .food(new FoodProperties.Builder().nutrition(4).saturationMod(3)
+                    .effect(()  -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 0), 1F)
+                    .build())));
+
+    //Register coke
+    public static final RegistryObject<Item> COKE = ITEMS.register("coke",
+            () -> new FuelItem(new Item.Properties().tab(OtherCreativeTab.other), 3200));
+
+    //Register Teleport staff
+    public static final RegistryObject<Item> TELEPORT_STAFF = ITEMS.register("teleport_staff",
+            () -> new TeleportStaff(new Item.Properties().tab(OtherCreativeTab.other)
+                    .durability(100)));
+
+    public static class RubyCreativeTab extends CreativeModeTab {
+        private RubyCreativeTab(int index, String label) {
             super(index, label);
         }
 
@@ -26,7 +48,22 @@ public class ItemInit {
             return new ItemStack(RUBY.get());
         }
 
-        public static final ModCreativeTab instance = new ModCreativeTab(CreativeModeTab.TABS.length, "ruby");
+        public static final RubyCreativeTab ruby = new RubyCreativeTab(CreativeModeTab.TABS.length, "ruby");
+
+
+    }
+
+    public static class OtherCreativeTab extends CreativeModeTab {
+        private OtherCreativeTab(int index, String label) {
+            super(index, label);
+        }
+
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(MANGO.get());
+        }
+
+        public static final OtherCreativeTab other = new OtherCreativeTab(CreativeModeTab.TABS.length, "other");
 
 
     }
